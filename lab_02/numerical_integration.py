3f4729fab5d9cd41c30111cb7751c8db9cb3242b
kosarevskiydp <kosarevskiydp@pik.ru>
2021-11-05 23:41:56 +0300

    v3

--- lab_02/numerical_integration.py
+++ lab_02/numerical_integration.py
@@ -1,244 +1,97 @@
 import matplotlib.pyplot as plt
 from math import sin, cos, pi, exp
 import streamlit as st
-import pandas as pd
 import numpy as np
-from math import fabs
-
-from st_aggrid import AgGrid
-
-
-# FIXME
-def mul_polynoms(pol1, pol2):
-    res_pol = [0 for i in range(len(pol1) + len(pol2) - 1)]
-
-    for i in range(len(pol1)):
-        for j in range(len(pol2)):
-            res_pol[i + j] += pol1[i] * pol2[j]
-
-    return res_pol
+from numpy import linspace, array
+from numpy.linalg import solve
+from typing import List
+from typing import Callable as func
 
 
-def diff_polynoms(pol1, pol2):
-    max_len = max(len(pol1), len(pol2))
-    res_pol = [0 for i in range(max_len)]
-
-    for i in range(len(pol1)):
-        res_pol[i + len(res_pol) - len(pol1)] += pol1[i]
-
-    for i in range(len(pol2)):
-        res_pol[i + len(res_pol) - len(pol2)] -= pol2[i]
-
-    return res_pol
-
-
-def polynom_value(pol, x):
-    res = 0
-    for i in range(len(pol)):
-        res += pol[i] * (x ** (len(pol) - i - 1))
-
-    return res
-
-
-def find_legandr_pol(n):
-    leg_pol_0 = [1]  # 1
-    leg_pol_1 = [1, 0]  # x
-
-    prev = leg_pol_0
-    res_pol = leg_pol_1
-
-    for power in range(2, n + 1):
-        coef_1 = [(2 * power - 1) / power, 0]  # (2 ^ power - 1 / power) * x
-        coef_2 = [(power - 1) / power]  # (power - 1) / power
-        mul_pol1 = mul_polynoms(coef_1, res_pol)
-        mul_pol2 = mul_polynoms(coef_2, prev)
-        tmp = [x for x in res_pol]
-        res_pol = diff_polynoms(mul_polynoms(coef_1, res_pol), mul_polynoms(coef_2, prev))
-        prev = [x for x in tmp]
-
-    return res_pol
-
-
-def half_mid_division(pol, left, right):
-    mid = left + (right - left) / 2
-    res = polynom_value(pol, mid)
-    while fabs(res) > 1e-5:
-        if res * polynom_value(pol, left) < 0:
-            right = mid
-        else:
-            left = mid
-        mid = left + (right - left) / 2
-        res = polynom_value(pol, mid)
-
-    return mid
-
-
-def find_roots(leg_pol):
-    n = len(leg_pol) - 1
-    parts = 2 * n
-    is_find_segments = False
-
-    while not is_find_segments:
-        segments = []
-        step = 2 / parts
-
-        x = -1
-        for i in range(parts - 1):
-            if polynom_value(leg_pol, x) * polynom_value(leg_pol, x + step) < 0 or polynom_value(leg_pol, x) == 0:
-                segments.append([x, x + step])
-            x += step
-        if polynom_value(leg_pol, x) * polynom_value(leg_pol, 1) < 0 or polynom_value(leg_pol, x) == 0:
-            segments.append([x, 1])
-
-        if len(segments) == n:
-            is_find_segments = True
-
-    return [half_mid_division(leg_pol, seg[0], seg[1]) for seg in segments]
-
-
-def solve_slau(slau):
-    for i in range(len(slau)):
-        tmp = slau[i][i]
-        for j in range(len(slau[0])):
-            slau[i][j] /= tmp
-
-        for j in range(i + 1, len(slau)):
-            tmp = slau[j][i]
-            for k in range(len(slau[0])):
-                slau[j][k] -= slau[i][k] * tmp
-
-    coefs = []
-
-    for i in range(len(slau) - 1, -1, -1):
-        coef = slau[i][len(slau[0]) - 1]
-
-        for j in range(len(coefs)):
-            coef -= coefs[j] * slau[i][i + j + 1]
-
-        coefs.insert(0, coef)
-
-    return coefs
-
-
-def find_args(roots):
-    mtr = []
-    for i in range(len(roots)):
-        row = [root ** i for root in roots]
-        if i % 2 == 1:
-            row.append(0)
-        else:
-            row.append(2 / (i + 1))
-        mtr.append(row)
-
-    return solve_slau(mtr)
-
-
-def convert_arg(t, a, b):
-    return (b - a) / 2 * t + (b + a) / 2
-
-
-def gauss(func, a, b, node_count):
-    leg_pol = find_legandr_pol(node_count)
-    roots = find_roots(leg_pol)
-    args = find_args(roots)
-
-    res = 0
-    for i in range(node_count):
-        res += (b - a) / 2 * args[i] * func(convert_arg(roots[i], a, b))
-
-    return res
-
-
-# FIXME
-def simpson(func, a, b, nodes_count):
-    h = (b - a) / (nodes_count - 1)
-    x = a
-    res = 0
-
-    for i in range((nodes_count - 1) // 2):
-        res += func(x) + 4 * func(x + h) + func(x + 2 * h)
-        x += 2 * h
-
+def simpson_integrate(f: func, a: float, b: float, n: int) -> float:
+    h, res = (b - a) / n, 0
+    for i in range(0, n, 2):
+        x1, x2, x3 = i * h, (i + 1) * h, (i + 2) * h
+        f1, f2, f3 = f(x1), f(x2), f(x3)
+        res += f1 + 4 * f2 + f3
     return h / 3 * res
 
 
-# FIXME
-def tau_func(tau):
-    sub_func = lambda phi, teta: ((2 * cos(teta)) / (1 - sin(teta) * sin(teta) * cos(phi) * cos(phi)))
-    return lambda phi, teta: (4 / pi * (1 - exp(-tau * sub_func(phi, teta))) * cos(teta) * sin(teta))
+# Возвращает значение полинома Лежандра n-го порядка
+def legendre(n: int, x: float) -> float:
+    if n < 2: return [1, x][n]
+    P1, P2 = legendre(n - 1, x), legendre(n - 2, x)
+    return ((2 * n - 1) * x * P1 - (n - 1) * P2) / n
 
 
-def func_2_to_1(func, value):
-    return lambda y: func(value, y)
+# возвращает значение производной полинома Лежандра
+def legendre_prime(n: int, x: float) -> float:
+    P1, P2 = legendre(n - 1, x), legendre(n, x)
+    return n / (1 - x * x) * (P1 - x * P2)
 
 
-def double_integreation(double_func, limits, nodes_counts, integreate_funcs):
-    F = lambda y: integreate_funcs[1](func_2_to_1(double_func, y), limits[1][0], limits[1][1], nodes_counts[1])
-    return integreate_funcs[0](F, limits[0][0], limits[0][1], nodes_counts[0])
+# Нахождение корней полинома Лежандра n-го порядка
+def legendre_roots(n: int, eps: float = 1e-12) -> List[float]:
+    roots = [cos(pi * (4 * i + 3) / (4 * n + 2)) for i in range(n)]
+    for i, root in enumerate(roots):  # уточнение корней
+        root_val = legendre(n, root)
+        while abs(root_val) > eps:
+            root -= root_val / legendre_prime(n, root)
+            root_val = legendre(n, root)
+        roots[i] = root
+    return roots
 
 
-def plot_graphic(integr_func, tau_start, tau_end, tau_step, label):
-    plt.figure(1)
+# Метод Гаусса для численного интегрирования на [-1; 1]
+def gauss_integrate_norm(f: func, n: int) -> float:
+    t = legendre_roots(n)
+    T = array([[t_i**k for t_i in t] for k in range(n)])
 
-    x = []
-    tau = tau_start
-    while tau <= tau_end:
-        x.append(tau)
-        tau += tau_step
+    int_tk = lambda k: 2 / (k + 1) if k % 2 == 0 else 0
+    b = array([int_tk(k) for k in range(n)])
+    A = solve(T, b)  # решение системы линейных уравнений
 
-    plt.plot(x, [integr_func(tau) for tau in x], label=label)
+    return sum(A_i * f(t_i) for A_i, t_i in zip(A, t))
 
-    plt.xlabel("tau")
-    plt.ylabel("Интеграция")
-    plt.grid(True)
-    plt.legend(loc=0)
+
+# Метод Гаусса для произвольного промежутка [a; b]
+def gauss_integrate(f: func, a: float, b: float, n: int) -> float:
+    mean, diff = (a + b) / 2, (b - a) / 2
+    g = lambda t: f(mean + diff * t)
+    return diff * gauss_integrate_norm(g, n)
+
+
+def composite_integrate(f: func, a1: float, b1: float, a2: float, b2: float,
+                        method_1: func, method_2: func, n1: int, n2: int) -> float:
+    F = lambda y: method_1(lambda x: f(x, y), a1, b1, n1)
+    return method_2(F, a2, b2, n2)
+
+
+def function_integrator(f: func, a: float, b: float, c: float, d: float, n: int, m: int) -> float:
+    return composite_integrate(f, a, b, c, d, gauss_integrate, simpson_integrate, n, m)
+
+
+def function(t: float, n: int, m: int) -> float:
+    L_R = lambda theta, phi: 2 * cos(theta) / (1 - sin(theta)**2 * cos(phi)**2)
+    f = lambda theta, phi: (1 - exp(-t * L_R(theta, phi))) * cos(theta) * sin(theta)
+
+    return 4 / pi * function_integrator(f, 0, pi / 2, 0, pi / 2, n, m)
+
+
+def test_function(func):
+    def test_f(x):
+        return 0.3 * (x - 1) ** 2 - 0.1 * (x + 4) ** 2 - x
+
+    def act_int_f(x):
+        return 0.1 * (x - 1) ** 3 - 0.1 / 3 * (x + 4) ** 3 - 0.5 * x ** 2
+
+    X = linspace(-10, 10, 100)
+    Y = act_int_f(X) - act_int_f(0 * X)
+    F = lambda n: [func(test_f, 0, x, n) for x in X]
+
+    plt.plot(X, Y)
+    plt.plot(X, F(1))
     st.pyplot(plt)
-
-
-def gen_label(func1, func2, N, M):
-    label = ""
-    if func1 == gauss:
-        label += "ext - gauss\n"
-    else:
-        label += "ext - simpson\n"
-    if func2 == gauss:
-        label += "int - gauss\n"
-    else:
-        label += "int - simpson\n"
-
-    label += f"ext nodes - {N}\nint nodes - {M}"
-
-    return label
-
-
-def test_simpson():
-    func1 = gauss
-    func2 = simpson
-
-    int_func1 = lambda tau: double_integreation(tau_func(tau), [[0, pi / 2], [0, pi / 2]], [10, 9], [func1, func2])
-    int_func2 = lambda tau: double_integreation(tau_func(tau), [[0, pi / 2], [0, pi / 2]], [10, 3], [func1, func2])
-    int_func3 = lambda tau: double_integreation(tau_func(tau), [[0, pi / 2], [0, pi / 2]], [9, 10], [func2, func1])
-    int_func4 = lambda tau: double_integreation(tau_func(tau), [[0, pi / 2], [0, pi / 2]], [3, 10], [func2, func1])
-
-    plot_graphic(int_func1, 0.05, 10, 0.05, gen_label(func1, func2, 10, 9))
-    plot_graphic(int_func2, 0.05, 10, 0.05, gen_label(func1, func2, 10, 3))
-    plot_graphic(int_func3, 0.05, 10, 0.05, gen_label(func2, func1, 9, 10))
-    plot_graphic(int_func4, 0.05, 10, 0.05, gen_label(func2, func1, 3, 10))
-
-
-def test_gauss():
-    func1 = gauss
-    func2 = simpson
-
-    int_func1 = lambda tau: double_integreation(tau_func(tau), [[0, pi / 2], [0, pi / 2]], [3, 9], [func1, func2])
-    int_func2 = lambda tau: double_integreation(tau_func(tau), [[0, pi / 2], [0, pi / 2]], [10, 9], [func1, func2])
-    int_func3 = lambda tau: double_integreation(tau_func(tau), [[0, pi / 2], [0, pi / 2]], [9, 3], [func2, func1])
-    int_func4 = lambda tau: double_integreation(tau_func(tau), [[0, pi / 2], [0, pi / 2]], [9, 10], [func2, func1])
-
-    plot_graphic(int_func1, 0.05, 10, 0.05, gen_label(func1, func2, 3, 9))
-    plot_graphic(int_func2, 0.05, 10, 0.05, gen_label(func1, func2, 10, 9))
-    plot_graphic(int_func3, 0.05, 10, 0.05, gen_label(func2, func1, 9, 3))
-    plot_graphic(int_func4, 0.05, 10, 0.05, gen_label(func2, func1, 9, 10))
 
 
 def main():
@@ -250,29 +103,52 @@ def main():
     st.write("---")
 
     c0, c1, c2 = st.columns(3)
-    tau = c0.number_input("Введите значение параметра τ:", min_value=.0, max_value=1., value=.545, format="%.3f")
-    N = c1.number_input("Введите значение N:", min_value=0, max_value=7, value=3, step=1)
-    M = c2.number_input("Введите значение M:", min_value=0, max_value=7, value=3, step=1)
+    tau = c0.number_input("Введите значение параметра τ:", min_value=0, max_value=1, value=1)
+    N = c1.number_input("Введите значение N:", min_value=0, max_value=100, value=3, step=1)
+    M = c2.number_input("Введите значение M:", min_value=0, max_value=100, value=12, step=1)
 
-    c3, c4 = st.columns(2)
-    ext_method = c3.selectbox("Выберите внешний метод", ("Гаусс", "Симпсон"))
-    int_method = c4.selectbox("Выберите внутренний метод", ("Гаусс", "Симпсон"))
-    func1 = gauss if ext_method == "Гаусс" else simpson
-    func2 = gauss if int_method == "Гаусс" else simpson
+    # c3, c4 = st.columns(2)
+    # ext_method = c3.selectbox("Выберите внешний метод", ("Гаусс", "Симпсон"))
+    # int_method = c4.selectbox("Выберите внутренний метод", ("Гаусс", "Симпсон"))
+    # func1 = gauss if ext_method == "Гаусс" else simpson
+    # func2 = gauss if int_method == "Гаусс" else simpson
 
-    integr_func = lambda tau_: double_integreation(tau_func(tau), [[0, pi/2], [0, pi/2]], [N, M], [func1, func2])
-    st.write(f"Результат интегрирования: {integr_func(tau)}")
+    # integr_func = lambda tau_: double_integreation(tau_func(tau), [[0, pi/2], [0, pi/2]], [N, M], [func1, func2])
+    integr_func = function(tau, N, M)
+    st.write(f"Результат интегрирования: {integr_func}")
 
-    test = st.selectbox("Выберите метод тестирования", ("Гаусс", "Симпсон", "Не тестировать"))
-    if test == "Не тестировать":
-        plot_graphic(integr_func, 0.05, 10, 0.01,
-                     gen_label(func1, func2, N, M))
-    elif test == "Симпсон":
-        test_simpson()
-    elif test == "Гаусс":
-        test_gauss()
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
 
+    major_ticks = linspace(0, 10, 6)
+    minor_ticks = linspace(0, 10, 21)
+
+    ax.set_xticks(major_ticks)
+    ax.set_xticks(minor_ticks, minor=True)
+    ax.set_yticks(linspace(0, 1, 6))
+    ax.set_yticks(linspace(0, 1, 11), minor=True)
+
+    # And a corresponding grid
+    ax.grid(which='both')
+
+    # Or if you want different settings for the grids:
+    ax.grid(which='minor', alpha=0.4)
+    ax.grid(which='major', alpha=0.8)
+
+    tao = linspace(0.05, 10, 100)
+    eps = [function(t, N, M) for t in tao]
+    ax.plot(tao, eps, label="ɛ(τ)")
+
+    plt.legend()
+    plt.xlabel("τ")
+    plt.ylabel("ɛ").set_rotation(0)
     st.pyplot(plt)
+
+    test = st.selectbox("Выберите метод тестирования", ("Гаусс", "Симпсон"))
+    if test == "Симпсон":
+        test_function(simpson_integrate)
+    elif test == "Гаусс":
+        test_function(gauss_integrate)
 
 
 if __name__ == "__main__":
