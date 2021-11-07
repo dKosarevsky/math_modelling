997fd82fe60d1b0c7bb956d20ded569ee1e7e87e
kosarevskiydp <kosarevskiydp@pik.ru>
2021-11-07 22:26:59 +0300

    fix simpson

--- lab_02/numerical_integration.py
+++ lab_02/numerical_integration.py
@@ -1,97 +1,125 @@
-import matplotlib.pyplot as plt
-from math import sin, cos, pi, exp
+import plotly.graph_objects as go
 import streamlit as st
 import numpy as np
-from numpy import linspace, array
-from numpy.linalg import solve
-from typing import List
-from typing import Callable as func
+
+from typing import List, Callable
 
 
-def simpson_integrate(f: func, a: float, b: float, n: int) -> float:
+def simpson_integrate(function: Callable, a: float, b: float, n: int) -> float:
+    """ интегрирование методом Симпсона """
     h, res = (b - a) / n, 0
-    for i in range(0, n, 2):
-        x1, x2, x3 = i * h, (i + 1) * h, (i + 2) * h
-        f1, f2, f3 = f(x1), f(x2), f(x3)
-        res += f1 + 4 * f2 + f3
-    return h / 3 * res
+    x = a
+    while x < b:
+        next_x = x + h
+        res += function(x) + 4 * function(x + h * .5) + function(next_x)
+        x = next_x
+    return h / 6 * res
 
 
-# Возвращает значение полинома Лежандра n-го порядка
-def legendre(n: int, x: float) -> float:
-    if n < 2: return [1, x][n]
-    P1, P2 = legendre(n - 1, x), legendre(n - 2, x)
-    return ((2 * n - 1) * x * P1 - (n - 1) * P2) / n
+def legendre_polynomial(n: int, x: float) -> float:
+    """ значение полинома Лежандра n-го порядка """
+    if n < 2:
+        return [1, x][n]
+    p1, p2 = legendre_polynomial(n - 1, x), legendre_polynomial(n - 2, x)
+    return ((2 * n - 1) * x * p1 - (n - 1) * p2) / n
 
 
-# возвращает значение производной полинома Лежандра
-def legendre_prime(n: int, x: float) -> float:
-    P1, P2 = legendre(n - 1, x), legendre(n, x)
-    return n / (1 - x * x) * (P1 - x * P2)
+def derivative_legendre_polynomial(n: int, x: float) -> float:
+    """ значение производной полинома Лежандра """
+    p1, p2 = legendre_polynomial(n - 1, x), legendre_polynomial(n, x)
+    return n / (1 - x * x) * (p1 - x * p2)
 
 
-# Нахождение корней полинома Лежандра n-го порядка
-def legendre_roots(n: int, eps: float = 1e-12) -> List[float]:
-    roots = [cos(pi * (4 * i + 3) / (4 * n + 2)) for i in range(n)]
+def roots_legendre_polynomial(n: int, eps: float = 1e-12) -> List[float]:
+    """ корни полинома Лежандра n-го порядка """
+    roots = [np.cos(np.pi * (4 * i + 3) / (4 * n + 2)) for i in range(n)]
     for i, root in enumerate(roots):  # уточнение корней
-        root_val = legendre(n, root)
+        root_val = legendre_polynomial(n, root)
         while abs(root_val) > eps:
-            root -= root_val / legendre_prime(n, root)
-            root_val = legendre(n, root)
+            root -= root_val / derivative_legendre_polynomial(n, root)
+            root_val = legendre_polynomial(n, root)
         roots[i] = root
     return roots
 
 
-# Метод Гаусса для численного интегрирования на [-1; 1]
-def gauss_integrate_norm(f: func, n: int) -> float:
-    t = legendre_roots(n)
-    T = array([[t_i**k for t_i in t] for k in range(n)])
+def gauss_integrate_norm(f: Callable, n: int) -> float:
+    """ интегрирование методом Гаусса на промежутке [-1, 1] """
+    t = roots_legendre_polynomial(n)
+    T = np.array([[t_i ** k for t_i in t] for k in range(n)])
 
-    int_tk = lambda k: 2 / (k + 1) if k % 2 == 0 else 0
-    b = array([int_tk(k) for k in range(n)])
-    A = solve(T, b)  # решение системы линейных уравнений
+    tk = lambda k: 2 / (k + 1) if k % 2 == 0 else 0
+    b = np.array([tk(k) for k in range(n)])
+    A = np.linalg.solve(T, b)  # решение СЛАУ
 
     return sum(A_i * f(t_i) for A_i, t_i in zip(A, t))
 
 
-# Метод Гаусса для произвольного промежутка [a; b]
-def gauss_integrate(f: func, a: float, b: float, n: int) -> float:
-    mean, diff = (a + b) / 2, (b - a) / 2
+def gauss_integrate(f: Callable, a: float, b: float, n: int) -> float:
+    """ интегрирование методом Гаусса на произвольном промежутке """
+    mean, diff = (a + b) * .5, (b - a) * .5
     g = lambda t: f(mean + diff * t)
     return diff * gauss_integrate_norm(g, n)
 
 
-def composite_integrate(f: func, a1: float, b1: float, a2: float, b2: float,
-                        method_1: func, method_2: func, n1: int, n2: int) -> float:
-    F = lambda y: method_1(lambda x: f(x, y), a1, b1, n1)
-    return method_2(F, a2, b2, n2)
+def composite_integrate(f: Callable, a1: float, b1: float, a2: float, b2: float,
+                        method_1: Callable, method_2: Callable, n1: int, n2: int) -> float:
+    func = lambda y: method_1(lambda x: f(x, y), a1, b1, n1)
+    return method_2(func, a2, b2, n2)
 
 
-def function_integrator(f: func, a: float, b: float, c: float, d: float, n: int, m: int) -> float:
+def function_integrator(f: Callable, a: float, b: float, c: float, d: float, n: int, m: int) -> float:
     return composite_integrate(f, a, b, c, d, gauss_integrate, simpson_integrate, n, m)
 
 
-def function(t: float, n: int, m: int) -> float:
-    L_R = lambda theta, phi: 2 * cos(theta) / (1 - sin(theta)**2 * cos(phi)**2)
-    f = lambda theta, phi: (1 - exp(-t * L_R(theta, phi))) * cos(theta) * sin(theta)
+def integrate_function(t: float, n: int, m: int) -> float:
+    l_r = lambda theta, phi: 2 * np.cos(theta) / (1 - np.sin(theta) ** 2 * np.cos(phi) ** 2)
+    f = lambda theta, phi: (1 - np.exp(-t * l_r(theta, phi))) * np.cos(theta) * np.sin(theta)
 
-    return 4 / pi * function_integrator(f, 0, pi / 2, 0, pi / 2, n, m)
+    return 4 / np.pi * function_integrator(f, 0, np.pi * .5, 0, np.pi * .5, n, m)
 
 
-def test_function(func):
+def plot(func: Callable, n: int, m: int, test: bool = False, test_func: Callable = None, test_func2: Callable = None):
+    """ отрисовка графика """
+    st.markdown("---")
+    x = np.arange(.05, 10, .001) if not test else np.arange(-10, 10, .001)
+    y = [func(t, n, m) for t in x] if not test else test_func2(x) - test_func2(0 * x)
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        x=x,
+        y=y,
+        mode='lines',
+    ))
+
+    if test:
+        def test_y(n_test):
+            return [func(test_func, 0, x_test, n_test) for x_test in x]
+
+        fig.add_trace(go.Scatter(
+            x=x,
+            y=test_y(1),
+            mode='lines',
+        ))
+
+    fig.update_layout(
+        title_text=f"График зависимости ε(τ)" if not test else "График тестирования",
+        xaxis_title="τ" if not test else "",
+        yaxis_title="ε" if not test else "",
+        showlegend=False
+    )
+
+    st.write(fig)
+    st.markdown("---")
+
+
+def test_function(func: Callable):
     def test_f(x):
-        return 0.3 * (x - 1) ** 2 - 0.1 * (x + 4) ** 2 - x
+        return .3 * (x - 1) ** 2 - .1 * (x + 4) ** 2 - x
 
     def act_int_f(x):
-        return 0.1 * (x - 1) ** 3 - 0.1 / 3 * (x + 4) ** 3 - 0.5 * x ** 2
+        return .1 * (x - 1) ** 3 - .1 / 3 * (x + 4) ** 3 - .5 * x ** 2
 
-    X = linspace(-10, 10, 100)
-    Y = act_int_f(X) - act_int_f(0 * X)
-    F = lambda n: [func(test_f, 0, x, n) for x in X]
-
-    plt.plot(X, Y)
-    plt.plot(X, F(1))
-    st.pyplot(plt)
+    plot(func, 0, 0, test=True, test_func=test_f, test_func2=act_int_f)
 
 
 def main():
@@ -103,46 +131,14 @@ def main():
     st.write("---")
 
     c0, c1, c2 = st.columns(3)
-    tau = c0.number_input("Введите значение параметра τ:", min_value=0, max_value=1, value=1)
-    N = c1.number_input("Введите значение N:", min_value=0, max_value=100, value=3, step=1)
-    M = c2.number_input("Введите значение M:", min_value=0, max_value=100, value=12, step=1)
+    tau = c0.number_input("Введите значение параметра τ:", min_value=.0, max_value=1., value=.808, format="%.3f")
+    N = c1.number_input("Введите значение N:", min_value=1, max_value=100, value=4, step=1)
+    M = c2.number_input("Введите значение M:", min_value=1, max_value=100, value=5, step=1)
 
-    # c3, c4 = st.columns(2)
-    # ext_method = c3.selectbox("Выберите внешний метод", ("Гаусс", "Симпсон"))
-    # int_method = c4.selectbox("Выберите внутренний метод", ("Гаусс", "Симпсон"))
-    # func1 = gauss if ext_method == "Гаусс" else simpson
-    # func2 = gauss if int_method == "Гаусс" else simpson
+    result = integrate_function(tau, N, M)
+    st.write(f"Результат интегрирования: {round(result, 5)}")
 
-    # integr_func = lambda tau_: double_integreation(tau_func(tau), [[0, pi/2], [0, pi/2]], [N, M], [func1, func2])
-    integr_func = function(tau, N, M)
-    st.write(f"Результат интегрирования: {integr_func}")
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-
-    major_ticks = linspace(0, 10, 6)
-    minor_ticks = linspace(0, 10, 21)
-
-    ax.set_xticks(major_ticks)
-    ax.set_xticks(minor_ticks, minor=True)
-    ax.set_yticks(linspace(0, 1, 6))
-    ax.set_yticks(linspace(0, 1, 11), minor=True)
-
-    # And a corresponding grid
-    ax.grid(which='both')
-
-    # Or if you want different settings for the grids:
-    ax.grid(which='minor', alpha=0.4)
-    ax.grid(which='major', alpha=0.8)
-
-    tao = linspace(0.05, 10, 100)
-    eps = [function(t, N, M) for t in tao]
-    ax.plot(tao, eps, label="ɛ(τ)")
-
-    plt.legend()
-    plt.xlabel("τ")
-    plt.ylabel("ɛ").set_rotation(0)
-    st.pyplot(plt)
+    plot(integrate_function, N, M)
 
     test = st.selectbox("Выберите метод тестирования", ("Гаусс", "Симпсон"))
     if test == "Симпсон":
