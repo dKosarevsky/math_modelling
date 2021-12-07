93d3e1cf029cec3b558e2edc291b873b3600e845
kosarevskiydp <kosarevskiydp@pik.ru>
2021-12-07 23:12:31 +0300

    lab4 init

--- lab_04/differentiation_boundary.py
+++ lab_04/differentiation_boundary.py
@@ -1,7 +1,87 @@
 import streamlit as st
-import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
 
-from typing import List
+
+def k(x, a, b):
+    """ коэффициенты теплопроводности и теплоотдачи """
+    return a / (x - b)
+
+
+def alpha(x, c, d):
+    return c / (x - d)
+
+
+def p(x, c, d, R):
+    return 2 * alpha(x, c, d) / R
+
+
+def f(x, c, d, T_0, R):
+    return 2 * alpha(x, c, d) * T_0 / R
+
+
+def plus_half(x, a, b, h):
+    """ метод средних """
+    return (k(x, a, b) + k(x + h, a, b)) / 2
+
+
+def minus_half(x, a, b, h):
+    """ метод средних """
+    return (k(x, a, b) + k(x - h, a, b)) / 2
+
+
+def A(x, a, b, h):
+    """ коэффициенты разностной схемы """
+    return plus_half(x, a, b, h) / h
+
+
+def B(x, a, b, c, d, R, h):
+    return A(x, a, b, h) + C(x, a, b, h) + p(x, c, d, R) * h
+
+
+def C(x, a, b, h):
+    return minus_half(x, a, b, h) / h
+
+
+def D(x, c, d, T_0, R, h):
+    return f(x, c, d, T_0, R) * h
+
+
+def left_boundary_condition(h, a, b, c, d, R, F_0, T_0):
+    """ левое граничное условие """
+    k_0 = plus_half(0, a, b, h) + h * h * (p(0, c, d, R) + p(h, c, d, R)) / 16 + h * h * p(0, c, d, R) / 4
+    M0 = -plus_half(0, a, b, h) + h * h * (p(0, c, d, R) + p(h, c, d, R)) / 16
+    P0 = h * F_0 + h * h / 4 * ((f(0, c, d, T_0, R) + f(h, c, d, T_0, R)) / 2 + f(0, c, d, T_0, R))
+    return k_0, M0, P0
+
+
+def right_boundary_condition(l, a, b, c, d, h, alpha_n, R, T_0):
+    """ правое граничное условие """
+    k_n = -minus_half(l, a, b, h) / h - alpha_n - p(l, c, d, R) * h / 4 - ((p(l, c, d, R) + p(l - h, c, d, R)) * h) / 16
+    MN = minus_half(l, a, b, h) / h - ((p(l, c, d, R) + p(l - h, c, d, R)) * h) / 16
+    PN = - alpha_n * T_0 - h * (f(l, c, d, T_0, R) + f(l - h, c, d, T_0, R) + f(l, c, d, T_0, R)) / 8
+    return k_n, MN, PN
+
+
+def plot(x: np.array, t: np.array):
+    """ отрисовка графика """
+    st.markdown("---")
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        x=x,
+        y=t,
+        mode='lines',
+    ))
+
+    fig.update_layout(
+        title_text=f"График зависимости температуры T(x) от координаты x:",
+        xaxis_title="Длина, см",
+        yaxis_title="Температура, K",
+    )
+
+    st.write(fig)
+    st.markdown("---")
 
 
 def main():
@@ -12,61 +92,63 @@ def main():
     Получение навыков разработки алгоритмов решения краевой задачи при реализации моделей, 
     построенных на ОДУ второго порядка.""")
 
-    # TODO read task: https://drive.google.com/file/d/1R022ThJfPbRxiwUXq3x9ZGEUex5_j_dz/view
+    a1, a2, a3 = st.columns(3)
+    b1, b2, b3 = st.columns(3)
+    c1, c2, c3 = st.columns(3)
+    d1, d2, d3 = st.columns(3)
 
-    # FIXME all:
-    # formula = r"""
-    # $$
-    # y = \frac{a_0x}{a_1 + a_2x}
-    # $$
-    # """
-    # st.markdown("""**Задание:**
-    #     Задана табличная (сеточная) функция.
-    #     Имеется информация, что закономерность представленная таблицей, может быть описана формулой""")
-    # st.write(formula)
-    # st.write("Параметры функции:")
+    k_0 = a1.number_input("k₀", min_value=0., max_value=1., value=.4)
+    k_n = b1.number_input("kₙ", min_value=0., max_value=1., value=.1)
+    alpha_0 = c1.number_input("α₀", min_value=0., max_value=1., value=.05)
+    alpha_n = d1.number_input("αₙ", min_value=0., max_value=1., value=.01)
 
-    # a1, a2, a3 = st.columns(3)
-    # b1, b2, b3 = st.columns(3)
-    # c1, c2, c3 = st.columns(3)
-    # d1, d2, d3 = st.columns(3)
-    # e1, e2, e3 = st.columns(3)
-    # f1, f2, f3 = st.columns(3)
-    #
-    # x1 = a1.number_input("x₁", min_value=1, max_value=10, value=1, step=1)
-    # x2 = b1.number_input("x₂", min_value=1, max_value=10, value=2, step=1)
-    # x3 = c1.number_input("x₃", min_value=1, max_value=10, value=3, step=1)
-    # x4 = d1.number_input("x₄", min_value=1, max_value=10, value=4, step=1)
-    # x5 = e1.number_input("x₅", min_value=1, max_value=10, value=5, step=1)
-    # x6 = f1.number_input("x₆", min_value=1, max_value=10, value=6, step=1)
-    #
-    # y1 = a2.number_input("y₁:", min_value=.001, max_value=10., value=.571, format="%.4f")
-    # y2 = b2.number_input("y₂", min_value=.001, max_value=10., value=.889, format="%.4f")
-    # y3 = c2.number_input("y₃", min_value=.001, max_value=10., value=1.091, format="%.4f")
-    # y4 = d2.number_input("y₄", min_value=.001, max_value=10., value=1.231, format="%.4f")
-    # y5 = e2.number_input("y₅", min_value=.001, max_value=10., value=1.333, format="%.4f")
-    # y6 = f2.number_input("y₆", min_value=.001, max_value=10., value=1.412, format="%.4f")
-    #
-    # h = a3.number_input("h:", min_value=1, max_value=10, value=1, step=1)
-    #
-    # x_array = [x1, x2, x3, x4, x5, x6]
-    # y_array = [y1, y2, y3, y4, y5, y6]
-    # x_array_len = len(x_array)
-    # x_array_range = range(x_array_len)
-    #
-    # st.write("---")
-    # st.write("Итоговая таблица:")
-    # result_data = {
-    #     "x": x_array,
-    #     "y": y_array,
-    #     "1": [one_sided_derivative(ln, x_array_len, y_array, h) for ln in x_array_range],
-    #     "2": [central_derivative(ln, x_array_len, y_array, h) for ln in x_array_range],
-    #     "3": [runge_derivative(ln, x_array_len, y_array, h) for ln in x_array_range],
-    #     "4": [alignment_variables(ln, x_array_len, y_array, x_array) for ln in x_array_range],
-    #     "5": [second_derivative(ln, x_array_len, y_array, h) for ln in x_array_range],
-    # }
-    # result = pd.DataFrame(data=result_data).applymap("{0:.4f}".format)
-    # st.table(result.assign(hack="").set_index("hack"))
+    l = a2.number_input("l:", min_value=1, max_value=100, value=10)
+    T_0 = b2.number_input("T₀", min_value=1, max_value=1000, value=300)
+    R = c2.number_input("R", min_value=0., max_value=1., value=.5)
+    F_0 = d2.number_input("F₀", min_value=0, max_value=10, value=0)
+
+    h = a3.number_input("h:", min_value=.00001, max_value=1., value=.0001, format="%.5f")
+
+    # параметры коэффициентов теплопроводности и теплоотдачи
+    b = (k_n * l) / (k_n - k_0)
+    a = -k_0 * b
+    d = (alpha_n * l) / (alpha_n - alpha_0)
+    c = -alpha_0 * d
+
+    k_0, M0, P0 = left_boundary_condition(h, a, b, c, d, R, F_0, T_0)
+    k_n, MN, PN = right_boundary_condition(l, a, b, c, d, h, alpha_n, R, T_0)
+
+    # прямой ход
+    # массивы прогоночных коэффициентов
+    eps = [0]
+    eta = [0]
+
+    eps1 = -M0 / k_0
+    eta1 = P0 / k_0
+
+    eps.append(eps1)
+    eta.append(eta1)
+
+    x = h
+    n = 1
+    while x + h < l:
+        eps.append(C(x, a, b, h) / (B(x, a, b, c, d, R, h) - A(x, a, b, h) * eps[n]))
+        eta.append((A(x, a, b, h) * eta[n] + D(x, c, d, T_0, R, h)) / (B(x, a, b, c, d, R, h) - A(x, a, b, h) * eps[n]))
+        n += 1
+        x += h
+
+    # обратный ход
+    t = [0] * (n + 1)
+
+    # значение функции в последней точке
+    t[n] = (PN - MN * eta[n]) / (k_n + MN * eps[n])
+
+    for i in range(n - 1, -1, -1):
+        t[i] = eps[i + 1] * t[i + 1] + eta[i + 1]
+
+    x = [i for i in np.arange(0, l, h)]
+
+    plot(x, t[: -1])
 
 
 if __name__ == "__main__":
