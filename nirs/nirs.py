a7539e58b44a749b4cb924cb6eeed861e2776bd0
kosarevskiydp <kosarevskiydp@pik.ru>
2021-12-20 00:09:36 +0300

    upd main logic

--- nirs/nirs.py
+++ nirs/nirs.py
@@ -1,6 +1,6 @@
-import streamlit as st
 import numpy as np
 import plotly.graph_objects as go
+import streamlit as st
 
 
 def plot(x: np.array, t: np.array):
@@ -24,6 +24,66 @@ def plot(x: np.array, t: np.array):
     st.markdown("---")
 
 
+def equation_0(x):
+    """f(x) = sin(x)"""
+    return np.sin(x)
+
+
+def equation_1(x):
+    """f(x) = 3 + 2x - 3x² + 2x³"""
+    return 3 + 2 * x - 3 * x ** 2 + 2 * x ** 3
+
+
+def equation_2(x):
+    """f(x) = 1 / (3cos(x) + 2)"""
+    return 1 / (3 * np.cos(x) + 2)
+
+
+def equation_3(x, y):
+    """f(x, y) = 4 + 2y + 2x + xy + x³y"""
+    return 4 + 2 * y + 2 * x + x * y + x ** 3 * y
+
+
+def equation_4(x, y):
+    """f(x, y) = y²/x²"""
+    return y ** 2 / x ** 2
+
+
+def generate_random(a, b, num_samples):
+    return np.random.uniform(a, b, num_samples)
+
+
+def calculate_average(list_, num_samples, func):
+    sum_ = 0;
+    for i in range(0, num_samples):
+        sum_ += func(list_[i])
+
+    return sum_ / num_samples
+
+
+def run(a, b, num_samples, func):
+    list_ = generate_random(a, b, num_samples)
+
+    average = calculate_average(list_, num_samples, func)
+
+    integral_ = (b - a) * average
+
+    st.write(f"{integral_}")
+
+    return integral_
+
+
+def integral(a, b, num_samples, num_iter, func):
+    avg_sum = 0
+    for i in range(0, num_iter):
+        avg_sum += run(a, b, num_samples, func)
+    avg_integral = avg_sum / num_iter
+    st.write(f"""
+        Интеграл функции {func.__doc__} на интервале от {a} до {b} 
+        с использованием {num_samples} примеров и {num_iter} итераций равен: {avg_integral}
+    """)
+
+
 def main():
     st.markdown("### НИРС")
     st.markdown("""**Тема:**
@@ -44,9 +104,67 @@ def main():
     4. f(x, y) = y²/x²
     """
 
-    show_schema = st.checkbox("Показать функции")
+    show_schema = st.checkbox("Показать описание")
     if show_schema:
-        st.code(functions)
+        st.code(description)
+
+    st.markdown("---")
+    selected_fun = st.radio("Выберите функцию", (
+        f"0. {equation_0.__doc__}",
+        f"1. {equation_1.__doc__}",
+        f"2. {equation_2.__doc__}",
+        f"3. {equation_3.__doc__}",
+        f"4. {equation_4.__doc__}",
+    ))
+
+    function = ""
+    if selected_fun[:1] == "1":
+        function = equation_1
+    elif selected_fun[:1] == "2":
+        function = equation_2
+    elif selected_fun[:1] == "3":
+        function = equation_3
+    elif selected_fun[:1] == "4":
+        function = equation_4
+    elif selected_fun[:1] == "0":
+        function = equation_0
+
+    st.markdown("---")
+    c1, c2, c3 = st.columns(3)
+    c4, c5 = st.columns(2)
+    c6, c7 = st.columns(2)
+    a = c1.number_input("Введите нижний предел:", value=-1.)
+    b = c2.number_input("Введите верхний предел:", value=1.)
+    sub_intervals = c3.number_input("Введите шаг:", min_value=.00000000001, value=.01, format="%.8f")
+    precision = c4.number_input("Введите точность:", value=.1, format="%.8f")
+    epsilon = c5.number_input("Введите эпсилон (ε):", min_value=.00000000001, value=1.0 * (10 ** - 8), format="%.8f")
+    num_samples = c6.number_input("Введите количество примеров:", min_value=1, max_value=10, value=5, step=1)
+    num_iterations = c7.number_input("Введите количество итераций:", min_value=1, max_value=10000, value=100, step=1)
+
+    st.markdown("---")
+    # calc_type = st.radio(
+    #     "Выберите тип вычисления", (
+    #         "1. Вычисление однократного интеграла",
+    #         "2. Вычисление двойного интеграла",
+    #     )
+    # )
+    # if calc_type[:1] == "1":
+    #     st.write(calc_type[3:])
+    #
+    # elif calc_type[:1] == "2":
+    #     st.write(calc_type[3:])
+
+    if abs(a % np.pi) < epsilon:
+        a = int(a/np.pi) * np.pi
+    elif abs(a % np.e) < epsilon:
+        a = int(a/np.e) * np.e
+    if abs(b % np.pi) < epsilon:
+        b = int(b/np.pi) * np.pi
+    elif abs(b % np.e) < epsilon:
+        b = int(b/np.e) * np.e
+
+    st.write(f"Вычисление интеграла функции {function.__doc__}:")
+    integral(a, b, num_samples, num_iterations, function)
 
 
 if __name__ == "__main__":
